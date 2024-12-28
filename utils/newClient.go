bec5a2827461f8cad2c3db200e38684f8b905361
rotieno <rabinyitzahck@gmail.com>
2024-12-28 17:51:59 +0300

    feat: read and validate name then add client to the chat

--- utils/newClient.go
+++ utils/newClient.go
@@ -1,7 +1,10 @@
 package utils
 import(
+	"fmt"
 	"time"
 	"net"
+	"log"
+	"strings"
 )
 
 func clientConnection(connection net.Conn){
@@ -9,4 +12,43 @@ func clientConnection(connection net.Conn){
 	
 	DisplayLogo(connection)
 	connection.Write([]byte("[ENTER YOUR NAME]: "))
+
+	buffer := make([]byte, 1024)
+	num, err := connection.Read(buffer)
+	if err != nil{
+		log.Printf("Error reading client name: %v\n", err)
+		connection.Close()
+		return
+	}
+
+	connection.SetDeadline(time.Time{})
+
+	clientName := strings.TrimSpace(string(buffer[:num]))
+	if clientName == ""{
+		connection.Write([]byte("Name must not be empty\n"))
+		connection.Close()
+		return
+	}
+
+	mClients.Lock()
+	for _, client := range clients{
+		if strings.EqualFold(client.name, clientName){
+			mClients.Unlock()
+			connection.Write([]byte("The name has already been taken by another user\n"))
+			connection.Close()
+			return
+		}
+	}
+	mClients.Unlock()
+
+	newClient := &client{
+		name: clientName,
+		connection: connection,
+	}
+	mClients.Lock()
+	clients = append(clients, newClient)
+	mClients.Unlock()
+
+	connection.Write([]byte(fmt.Sprintf("Welcome to the chat, %s!\n",clientName )))
+
 }
