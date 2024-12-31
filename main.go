e5c29753705be20ee1f2b4b7716e7b6a33c154c2
rotieno <rabinyitzahck@gmail.com>
2024-12-31 11:28:29 +0300

    feat(main): run the program based on the port number

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+import(
+	"os"
+	"log"
+	"strconv"
+	"netcat/utils"
+)
+
+const defaultPort = ":8989"
+
+func main(){
+	switch len(os.Args){
+	case 1:
+		utils.Server(defaultPort)
+	case 2:
+		port, err := strconv.Atoi(os.Args[1])
+		if err != nil{
+			log.Printf("Error converting %q to an int: %v\n", port, err)
+			return
+		}
+
+		if port < 1024 || port > 65535{
+			log.Println("Invalid port. Allowed range is 1024 - 65535")
+			return
+		}
+		utils.Server(":" + strconv.Itoa(port))
+	default:
+		log.Println("[USAGE]: ./TCPChat $port")
+
+	}
+}
