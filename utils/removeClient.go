a969ccba07ebaf9bd1d0e0168dd3e985f08ff0d2
skisenge <skisengese@gmail.com>
2025-01-10 05:53:51 +0300

    fix: refactor code and formatting

--- utils/removeClient.go
+++ utils/removeClient.go
@@ -1,17 +1,18 @@
 package utils
-import(
+
+import (
 	"net"
 )
 
 // RemoveClient removes a specific client from the list of connected clients
 // It gets the correct client by checking the client's connection that matches
 // the passed connection.
-func RemoveClient(connection net.Conn){
+func RemoveClient(connection net.Conn) {
 	mClients.Lock()
 	defer mClients.Unlock()
 
-	for i, client := range clients{
-		if client.connection == connection{
+	for i, client := range clients {
+		if client.connection == connection {
 			client.connection.Close()
 			clients = append(clients[:i], clients[i+1:]...)
 			break
