1d495a186569f32a82b54749ce1ea94f599f5ce1
skisenge <skisengese@gmail.com>
2025-01-10 05:52:41 +0300

    fix: fix code formarting

--- utils/broadcast.go
+++ utils/broadcast.go
@@ -1,25 +1,24 @@
 package utils
-import(
+
+import (
 	"fmt"
-	"net"
 	"log"
-	"time"	
+	"net"
+	"time"
 )
 
 // BroadcastMessage sends a client's message to all other clients connected to the network
 // (with the exception of the sender)
-func BroadcastMessage(message string, senderConnection net.Conn){
-
-
+func BroadcastMessage(message string, senderConnection net.Conn) {
 	mClients.Lock()
 	defer mClients.Unlock()
 
-	for _, client := range clients{
-		if client.connection != senderConnection{ // exclude sender
+	for _, client := range clients {
+		if client.connection != senderConnection { // exclude sender
 			_, err := client.connection.Write([]byte(message))
 			client.connection.Write([]byte(fmt.Sprintf("\n[%s][%s]:", time.Now().Format("2006-01-02 15:04:05"),
-			client.name)))
-			if err != nil{
+				client.name)))
+			if err != nil {
 				log.Printf("Error broadcasting message to %v: %v\n", &client.name, err)
 				go RemoveClient(client.connection)
 			}
