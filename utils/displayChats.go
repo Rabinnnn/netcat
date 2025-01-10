412f3a0d6b09c754be5e7bd12675c6be57fec768
skisenge <skisengese@gmail.com>
2025-01-10 05:53:06 +0300

    fix: fix code formatting

--- utils/displayChats.go
+++ utils/displayChats.go
@@ -1,12 +1,12 @@
 package utils
 
 // DisplayChats sends chat history to the specified client
-func DisplayChats(client *client){
+func DisplayChats(client *client) {
 	mChatHistory.Lock()
 	defer mChatHistory.Unlock()
 
-	if len(chatHistory) > 0{
-		for _, chat := range chatHistory{
+	if len(chatHistory) > 0 {
+		for _, chat := range chatHistory {
 			client.connection.Write([]byte(chat))
 		}
 		client.connection.Write([]byte("\n"))
